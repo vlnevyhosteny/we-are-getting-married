1fc26ed531380e953fc68117ed751b2ad8781554
Vladimir Nevyhosteny <vnevyhosteny@gmail.com>
2021-02-19 13:27:46 +0100

    draft: opening img

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,22 @@
-import { Card, Col, Container, Row } from 'react-bootstrap';
-// import styles from '../styles/Home.module.scss';
+import { Col, Container, Row } from 'react-bootstrap';
+import Image from 'next/image';
+import styles from '../styles/Home.module.scss';
 
 export default function Home(): JSX.Element {
   return (
     <Container fluid>
       <Row>
         <Col className="utils-padding-none">
-          <Card>
-            <Card.Img src="couple_north_see.jpg" />
-            <Card.ImgOverlay>
-              <Card.Title>We are getting married!</Card.Title>
-            </Card.ImgOverlay>
-          </Card>
+          <div className={styles['home-opening-image-container']}>
+            <Image
+              src="/couple_north_see.jpg"
+              alt="Picture of the author"
+              layout="fill"
+              objectFit="cover"
+              objectPosition="center right"
+              quality={95}
+            />
+          </div>
         </Col>
       </Row>
     </Container>
